0c94a0ce0465338640211ffd479fcd7c3af8d510
Pheno John Wang <snswordsman@163.com>
2016-06-18 15:07:26 -0400

    delete word "codes"

--- HW3/CalAccuracy.m
+++
@@ -1,8 +0,0 @@
-function [ accuracy ] = CalAccuracy( w,x,y )
-% Calculate the accuracy
-result = exp(w*x');
-[max1,predict] = max(result);
-correct = sum(predict == y');
-accuracy = correct/length(y);
-end
-
--- HW3/prob_y_x.m
+++
@@ -1,9 +0,0 @@
-function [ prob ] = prob_y_x( k,w,x,i )
-%P(Y=k|X=xi)
-expSum = 0;
-for l = 1:size(w,1)
-    expSum = expSum + exp(w(l,:)*x(i,:)');
-end
-prob = exp(w(k,:)*x(i,:)') / expSum;
-end
-
